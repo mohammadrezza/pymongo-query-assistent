e45a5383905844518dd5972f007d458ea70ad110
Mohammadreza Mirhajianmoghadam <mrmirhajian1996@gmail.com>
2019-04-11 00:45:46 +0430

    added UserGender enum & inherited User from BaseModel

--- assistant_app/Models/User.py
+++ assistant_app/Models/User.py
@@ -1,9 +1,16 @@
-class User():
+from assistant_app.Models.Base import BaseModel
+
+from assistant_app.Models.UserGender import UserGender
+
+
+class User(BaseModel):
     def __init__(self, id_=-1):
+        BaseModel.__init__(self, id_)
         self.full_name: str = ""
         self.username: str = ""
         self.pwd: str = ""
         self.auth: str = ""
+        self.gender: UserGender = UserGender.Unknown
         self.img: str = ""
         self.last_login: str = ""
 
