0e51ce0473aaa7845b047ed02e5a7ad6e4de6bbb
Mohammadreza Mirhajianmoghadam <mrmirhajian1996@gmail.com>
2019-04-14 01:09:20 +0430

    changed model names & added collections creation

--- assistant_app/Models/Order.py
+++ assistant_app/Models/Order.py
@@ -1,13 +1,4 @@
-from assistant_app.Models.Base import BaseModel
-
-
-class Order(BaseModel):
-    def __init__(self, id_=-1):
-        BaseModel.__init__(self, id_)
-        self.transaction_id: str = ""
-        self.total_price: int = -1
-        self.products: list = []
-
-    @staticmethod
-    def get_collection_name():
-        return "Orders"
+class Order:
+    def __init__(self, food_id, quantity=1):
+        self.food_id = food_id
+        self.quantity = quantity
--- assistant_app/__init__.py
+++ assistant_app/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 
-
 app = Flask(__name__)
-app.config['MONGO_URI'] = "mongodb://localhost:27017/nobina"
+app.config['MONGO_URI'] = "mongodb://localhost:27017/DummyDB"
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def main():
     return "it's working !"
+
+
+@app.route("/install")
+def install():
+    collections = ["Users", "Products", "Bills"]
+    for coll in collections:
+        mongo.db.create_collection(coll)
 
 
 if __name__ == "__main__":
